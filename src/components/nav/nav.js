39c3cb7932ddd52a656f3a2e9cef03015a9401d5
Matthew Courtney <mcourtdev@gmail.com>
2022-06-06 02:41:21 -0400

    added in the nav bar with all the buttons need to build out functionality still

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -3,7 +3,22 @@ import './nav.scss'
 
 const Nav = () => {
   return (
-    <div className="nav">Wordle</div>
+    <div className="nav">
+      <div className="left-side-navs">
+        <ul className="nav-items">
+          <li className="item"><i class="fa-solid fa-bars"></i></li>
+          <li className="item"><i class="fa-solid fa-circle-question"></i></li>
+        </ul>
+        
+      </div>
+      <h3 className="wordle-title-nav">Wordle</h3>
+      <div className="right-side-navs">
+        <ul className="nav-items">
+          <li className="item"><i class="fa-solid fa-chart-simple"></i></li>
+          <li className="item"><i class="fa-solid fa-gear"></i></li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
