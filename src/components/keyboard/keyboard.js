8bf36bf9e2e135016b9c10680b907b21ea21d63f
Matthew Courtney <mcourtdev@gmail.com>
2022-06-05 22:55:40 -0400

    finally got the animations working how id like them.

--- src/components/keyboard/keyboard.js
+++ src/components/keyboard/keyboard.js
@@ -4,7 +4,25 @@ import './keyboard.scss'
 
 const Keyboard = () => {
 
-    const words = ['HELLO', 'FOCUS', 'SMART'];
+    const words = [ "CIGAR", "REBUT", "SISSY", "HUMPH", "AWAKE", "BLUSH", "FOCAL", "EVADE", "NAVAL", "SERVE", "HEATH", "DWARF", "MODEL", "KARMA", "STINK", "GRADE", "QUIET", "BENCH", "ABATE", "FEIGN", "MAJOR", "DEATH", "FRESH", "CRUST", "STOOL", "COLON", "ABASE", "MARRY", "REACT", "BATTY", "PRIDE", "FLOSS", "HELIX", "CROAK", "STAFF", "PAPER", "UNFED", "WHELP", "TRAWL", "OUTDO", 
+    "ADOBE", "CRAZY", "SOWER", "REPAY", "DIGIT", "CRATE", "CLUCK", "SPIKE", "MIMIC", "POUND", "MAXIM", "LINEN", "UNMET", "FLESH", "BOOBY", "FORTH", "FIRST", "STAND", "BELLY", "IVORY", "SEEDY", "PRINT", "YEARN", "DRAIN", "BRIBE", "STOUT", "PANEL", "CRASS", "FLUME", "OFFAL", "AGREE", "ERROR", "SWIRL", "ARGUE", "BLEED", "DELTA", "FLICK", "TOTEM", "WOOER", "FRONT", "SHRUB", "PARRY", "BIOME", "LAPEL", 
+    "START", "GREET", "GONER", "GOLEM", "LUSTY", "LOOPY", "ROUND", "AUDIT", "LYING", "GAMMA", "LABOR", "ISLET", "CIVIC", "FORGE", "CORNY", "MOULT", "SALAD", "AGATE", "SPICY", "SPRAY", "ESSAY", "FJORD", "SPEND", "KEBAB", "GUILD", "ABACK", "MOTOR", "ALONE", "HATCH", "HYPER", "THUMB", "DOWRY", "OUGHT", "BELCH", "DUTCH", "PILOT", "TWEED", "COMET", "JAUNT", "ENEMA", "STEED", "ABYSS",
+     "GROWL", "FLING", "DOZEN", "BOOZY", "ERODE", "WORLD", "GOUGE", "CLICK", "BRIAR", "GREAT", "ALTAR", "PULPY", "BLURT", "COAST", "DUCHY", "GROIN", "FIXER", "GROUP", "ROGUE", "BADLY", "SMART", "PITHY", "GAUDY", "CHILL", "HERON", "VODKA", "FINER", "SURER", "RADIO", "ROUGE", "PERCH", "RETCH", "WROTE", "CLOCK", "TILDE", "STORE", "PROVE", "BRING", "SOLVE", "CHEAT", "GRIME", "EXULT",
+      "USHER", "EPOCH", "TRIAD", "BREAK", "RHINO", "VIRAL", "CONIC", "MASSE", "SONIC", "VITAL", "TRACE", "USING", "PEACH", "CHAMP", "BATON", "BRAKE", "PLUCK", "CRAZE", "GRIPE", "WEARY", "PICKY", "ACUTE", "FERRY", "ASIDE", "TAPIR", "TROLL", "UNIFY", "REBUS", "BOOST", "TRUSS", "SIEGE", "TIGER", "BANAL", "SLUMP", "CRANK", "GORGE", "QUERY", "DRINK", "FAVOR", "ABBEY", "TANGY", "PANIC", 
+      "SOLAR", "SHIRE", "PROXY", "POINT", "ROBOT", "PRICK", "WINCE", "CRIMP", "KNOLL", "SUGAR", "WHACK", "MOUNT", "PERKY", "COULD", "WRUNG", "LIGHT", "THOSE", "MOIST", "SHARD", "PLEAT", "ALOFT", "SKILL", "ELDER", "FRAME", "HUMOR", "PAUSE", "ULCER", "ULTRA", "ROBIN", "CYNIC", "AGORA", "AROMA", "CAULK", "SHAKE", "PUPAL", "DODGE", "SWILL", "TACIT", "OTHER", "THORN", "TROVE", "BLOKE", 
+      "VIVID", "SPILL", "CHANT", "CHOKE", "RUPEE", "NASTY", "MOURN", "AHEAD", "BRINE", "CLOTH", "HOARD", "SWEET", "MONTH", "LAPSE", "WATCH", "FOCUS", "SMELT", "TEASE", "CATER", "MOVIE", "LYNCH", "SAUTE", "ALLOW", "RENEW", "THEIR", "SLOSH", "PURGE", "CHEST", "DEPOT", "EPOXY", "NYMPH", "FOUND", "SHALL", "HARRY", "STOVE", "LOWLY", "SNOUT", "TROPE", "FEWER", "SHAWL", "NATAL", "FIBRE", 
+      "COMMA", "FORAY", "SCARE", "STAIR", "BLACK", "SQUAD", "ROYAL", "CHUNK", "MINCE", "SLAVE", "SHAME", "CHEEK", "AMPLE", "FLAIR", "FOYER", "CARGO", "OXIDE", "PLANT", "OLIVE", "INERT", "ASKEW", "HEIST", "SHOWN", "ZESTY", "HASTY", "TRASH", "FELLA", "LARVA", "FORGO", "STORY", "HAIRY", "TRAIN", "HOMER", "BADGE", "MIDST", "CANNY", "FETUS", "BUTCH", "FARCE", "SLUNG", "TIPSY", "METAL", 
+      "YIELD", "DELVE", "BEING", "SCOUR", "GLASS", "GAMER", "SCRAP", "MONEY", "HINGE", "ALBUM", "VOUCH", "ASSET", "TIARA", "CREPT", "BAYOU", "ATOLL", "MANOR", "CREAK", "SHOWY", "PHASE", "FROTH", "DEPTH", "GLOOM", "FLOOD", "TRAIT", "GIRTH", "PIETY", "PAYER", "GOOSE", "FLOAT", "DONOR", "ATONE", "PRIMO", "APRON", "BLOWN", "CACAO", "LOSER", "INPUT", "GLOAT", "AWFUL", "BRINK", "SMITE", "BEADY", 
+      "RUSTY", "RETRO", "DROLL", "GAWKY", "HUTCH", "PINTO", "GAILY", "EGRET", "LILAC", "SEVER", "FIELD", "FLUFF", "HYDRO", "FLACK", "AGAPE", "WENCH", "VOICE", "STEAD", "STALK", "BERTH", "MADAM", "NIGHT", "BLAND", "LIVER", "WEDGE", "AUGUR", "ROOMY", "WACKY", "FLOCK", "ANGRY", "BOBBY", "TRITE", "APHID", "TRYST", "MIDGE", "POWER", "ELOPE", "CINCH", "MOTTO", "STOMP", "UPSET", "BLUFF", "CRAMP", 
+      "QUART", "COYLY", "YOUTH", "RHYME", "BUGGY", "ALIEN", "SMEAR", "UNFIT", "PATTY", "CLING", "GLEAN", "LABEL", "HUNKY", "KHAKI", "POKER", "GRUEL", "TWICE", "TWANG", "SHRUG", "TREAT", "UNLIT", "WASTE", "MERIT", "WOVEN", "OCTAL", "NEEDY", "CLOWN", "WIDOW", "IRONY", "RUDER", "GAUZE", "CHIEF", "ONSET", "PRIZE", "FUNGI", "CHARM", "GULLY", "INTER", "WHOOP", "TAUNT", "LEERY", "CLASS", "THEME", 
+      "LOFTY", "TIBIA", "BOOZE", "ALPHA", "THYME", "ECLAT", "DOUBT", "PARER", "CHUTE", "STICK", "TRICE", "ALIKE", "SOOTH", "RECAP", "SAINT", "LIEGE", "GLORY", "GRATE", "ADMIT", "BRISK", "SOGGY", "USURP", "SCALD", "SCORN", "LEAVE", "TWINE", "STING", "BOUGH", "MARSH", "SLOTH", "DANDY", "VIGOR", "HOWDY", "ENJOY", "VALID", "IONIC", "EQUAL", "UNSET", "FLOOR", "CATCH", "SPADE", "STEIN", "EXIST", 
+      "QUIRK", "DENIM", "GROVE", "SPIEL", "MUMMY", "FAULT", "FOGGY", "FLOUT", "CARRY", "SNEAK", "LIBEL", "WALTZ", "APTLY", "PINEY", "INEPT", "ALOUD", "PHOTO", "DREAM", "STALE", "VOMIT", "OMBRE", "FANNY", "UNITE", "SNARL", "BAKER", "THERE", "GLYPH", "POOCH", "HIPPY", "SPELL", "FOLLY", "LOUSE", "GULCH", "VAULT", "GODLY", "THREW", "FLEET", "GRAVE", "INANE", "SHOCK", "CRAVE", "SPITE", "VALVE", 
+      "SKIMP", "CLAIM", "RAINY", "MUSTY", "PIQUE", "DADDY", "QUASI", "ARISE", "DONTT", "REPOS", "AGING", "VALET", "OPIUM", "AVERT", "STUCK", "RECUT", "MULCH", "GENRE", "PLUME", "RIFLE", "COUNT", "INCUR", "TOTAL", "WREST", "MOCHA", "DETER", "STUDY", "LOVER", "SAFER", "RIVET", "FUNNY", "SMOKE", "MOUND", "UNDUE", "SEDAN", "PAGAN", "SWINE", "GUILE", "GUSTY", "EQUIP", "TOUGH", "CANOE", "CHAOS", 
+      "COVET", "HUMAN", "UDDER", "LUNCH", "BLAST", "STRAY", "MANGA", "MELEE", "LEFTY", "QUICK", "PASTE", "GIVEN", "OCTET", "RISEN", "GROAN", "LEAKY", "GRIND", "CARVE", "LOOSE", "SADLY", "SPILT", "APPLE", "SLACK", "HONEY", "FINAL", "SHEEN", "EERIE", "MINTY", "SLICK", "DERBY", "WHARF", "SPELT", "COACH", "ERUPT", "SINGE", "PRICE", "SPAWN", "FAIRY", "JIFFY", "FILMY", "STACK", "CHOSE", "SLEEP", 
+      "ARDOR", "NANNY", "NIECE", "WOOZY", "HANDY", "GRACE", "DITTO", "STANK", "CREAM", "USUAL", "DIODE", "VALOR", "ANGLE", "NINJA", "MUDDY", "CHASE", "REPLY", "PRONE", "SPOIL", "HEART", "SHADE", "DINER", "ARSON", "ONION", "SLEET", "DOWEL", "COUCH", "PALSY", "BOWEL", "SMILE", "EVOKE", "CREEK", "LANCE", "EAGLE", "IDIOT", "SIREN", "BUILT", "EMBED", "AWARD", "DROSS", "ANNUL", "GOODY", "FROWN", 
+      "PATIO", "LADEN", "HUMID", "ELITE", "LYMPH", "EDIFY", "MIGHT", "RESET", "VISIT", "GUSTO", "PURSE", "VAPOR", "CROCK", "WRITE", "SUNNY", "LOATH", "CHAFF", "SLIDE", "QUEER", "VENOM", "STAMP", "SORRY", "STILL", "ACORN", "APING", "PUSHY", "TAMER", "HATER", "MANIA", "AWOKE", "BRAWN", "SWIFT", "EXILE", "BIRCH", "LUCKY", "FREER", "RISKY", "GHOST", "PLIER", "LUNAR", "WINCH", "SNARE", "NURSE", 
+      "HOUSE", "BORAX", "NICER", "LURCH", "EXALT", "ABOUT", "SAVVY", "TOXIN", "TUNIC", "PRIED", "INLAY", "CHUMP", "LANKY", "CRESS", "EATER", "ELUDE", "CYCLE", "KITTY", "BOULE", "MORON", "TENET", "PLACE", "LOBBY", "PLUSH", "VIGIL", "INDEX", "BLINK", "CLUNG", "QUALM", "CROUP", "CLINK", "JUICY", "STAGE", "DECAY", "NERVE", "FLIER", "SHAFT", "CROOK", "CLEAN", "CHINA", "RIDGE", "VOWEL", "GNOME", 
+      "SNUCK", "ICING", "SPINY", "RIGOR", "SNAIL", "FLOWN", "RABID", "PROSE", "THANK", "POPPY", "BUDGE", "FIBER", "MOLDY", "DOWDY", "KNEEL", "TRACK", "CADDY", "QUELL", "DUMPY", "PALER", "SWORE", "REBAR", "SCUBA", "SPLAT", "FLYER", "HORNY", "MASON", "DOING", "OZONE", "AMPLY"]
 
     let gc = window.localStorage.getItem('GAME_COUNT') || 0;
     let gw = window.localStorage.getItem('GAMES_WON') || 0;
@@ -30,6 +48,7 @@ const Keyboard = () => {
 
 
     let word = words[gameCount];
+    console.log(word)
 
     const keyClick = (e) => {
         let currentRow = document.getElementById(rowIterator);
@@ -53,15 +72,16 @@ const Keyboard = () => {
                 let key = document.getElementById(letter);
                 if (letter === wordArr[i]) {
                     answerLetters[i].classList.add('green');
-                    if(!key.classList.contains('green')) {
-                        key.classList.add('green')
+                    if(!key.classList.contains('green-key')) {
+                        key.classList.add('green-key')
                     }
                 } else if (letter !== wordArr[i] && wordArr.includes(letter)) {
                     answerLetters[i].classList.add('yellow');
-                    if (!key.classList.contains('yellow')) {
-                        key.classList.add('yellow')
+                    if (!key.classList.contains('yellow-key')) {
+                        key.classList.add('yellow-key')
                     }
                 } else {
+                    answerLetters[i].classList.add('gray');
                     if (!key.classList.contains('grey')) {
                         key.classList.add('grey')
                     }
@@ -99,15 +119,16 @@ const Keyboard = () => {
                 let key = document.getElementById(letter);
                 if (letter === wordArr[i]) {
                     answerLetters[i].classList.add('green');
-                    if(!key.classList.contains('green')) {
-                        key.classList.add('green')
+                    if(!key.classList.contains('green-key')) {
+                        key.classList.add('green-key')
                     }
                 } else if (letter !== wordArr[i] && wordArr.includes(letter)) {
                     answerLetters[i].classList.add('yellow');
-                    if (!key.classList.contains('yellow')) {
-                        key.classList.add('yellow')
+                    if (!key.classList.contains('yellow-key')) {
+                        key.classList.add('yellow-key')
                     }
                 } else {
+                    answerLetters[i].classList.add('gray');
                     if (!key.classList.contains('grey')) {
                         key.classList.add('grey')
                     }
--- src/components/wordle-row/wordle-row.js
+++ src/components/wordle-row/wordle-row.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './wordle-row.css'
 import './wordle-row.scss'
 
 const WordleRow = ({id}) => {
   return (
     <div className="row" id={id}>
-        <div className="letter-entry"></div>
-        <div className="letter-entry"></div>
-        <div className="letter-entry"></div>
-        <div className="letter-entry"></div>
-        <div className="letter-entry"></div>
+        <div className="letter-entry" data-count="1"></div>
+        <div className="letter-entry" data-count="2"></div>
+        <div className="letter-entry" data-count="3"></div>
+        <div className="letter-entry" data-count="4"></div>
+        <div className="letter-entry" data-count="5"></div>
     </div>
   )
 }
